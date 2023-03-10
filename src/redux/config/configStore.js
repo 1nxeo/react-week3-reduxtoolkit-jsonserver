40d5f05a7f4f6ce5455db4db50144719782bd2e7
Inseo Yang <y.1nseooo@gmail.com>
2023-03-10 11:21:19 +0900

    first commit

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,15 +1,21 @@
-// 중앙 데이터 관리소 (State)를 설정하는 부분
+import { configureStore } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+// import { combineReducers } from "redux";
+import todos from "../modules/todos";
 
-import {createStore} from "redux"
-import { combineReducers } from "redux"
-import counter from "../modules/counter"
-import users from "../modules/user"
+// 1. create rootReducer with reducers
+// const rootReducer = combineReducers({
+//   todos,
+// });
 
+// 2. create store
+// const store = createStore(rootReducer);
 
-const rootReducer = combineReducers({
-    //modules 내의 state의 묶음들을 객체 형태로 몰아 넣어줄 것이다.
-    counter, users
+const store = configureStore({
+  reducer:{
+    todos,
+  }
 })
-const store = createStore(rootReducer)
 
-export default store
+// 3. export
+export default store;
